c3cee423f86bc4baf3ea8af7154f7da0014e7343
MauCoden95 <migmauro95@gmail.com>
2026-04-13 17:51:44 -0300

    feat: implement user creation endpoint, DTOs, and entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { password, ...userData } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        ...userData,
+        password: hashedPassword,
+      },
+    });
+
+    
+    const { password: _, ...result } = user;
+    return result;
+  }
 
   async findOneByEmail(email: string) {
     return this.prisma.user.findUnique({
@@ -17,3 +35,4 @@ export class UsersService {
     });
   }
 }
+
